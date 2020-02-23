4ad9c714a271568205372244d6d1b4f55f6266de
Ebisiemen Ajayi <ebisiemen@gmail.com>
2020-02-23 20:35:59 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,11 +1,14 @@
-def key_for_min_value(name_hash)
-  lowest_key = nil 
-  lowest_value = nil 
-  name_hash.each do |key, value|
-    if lowest_value == nil || value < lowest_value
-      lowest_value = value
-      lowest_key = key 
-    end
+require 'pry'
+
+def key_for_min_value(hash) 
+  lowest_value_key = nil
+  lowest_value = 0
+  hash.each do |k,v| 
+    if v < lowest_value || lowest_value_key == nil
+     lowest_value_key = k 
+     lowest_value = v
+    # binding.pry
+   end 
   end 
-    lowest_key
-end
+  lowest_value_key
+end   
